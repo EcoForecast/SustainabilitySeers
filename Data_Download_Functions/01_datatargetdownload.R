aa276a6451fa3d0ed0c34155d6a889a4b54f9ed8
bvanloen <157618334+bvanloen@users.noreply.github.com>
2024-03-07 22:11:06 -0500

    added additional NOAA variables

--- Data_Download_Functions/01_datatargetdownload.R
+++ Data_Download_Functions/01_datatargetdownload.R
@@ -13,7 +13,6 @@ download_targets <- function(){
   readr::read_csv("https://sdsc.osn.xsede.org/bio230014-bucket01/challenges/targets/project_id=neon4cast/duration=PT30M/terrestrial_30min-targets.csv.gz", guess_max = 1e6)
 }
 
-
 ##' Download Site metadata
 ##' @return metadata dataframe
 download_site_meta <- function(){
@@ -42,49 +41,52 @@ download_met_forecast <- function(forecast_date){
   ## filter available forecasts by date and variable
   met_future <- df_future |> 
     dplyr::filter(datetime >= lubridate::as_datetime(forecast_date), 
-                  variable == "air_temperature") |> 
+                  variable %in% c("air_pressure", "air_temperature",
+                                  "relative_humidity", "preciptation_flux")) |> 
     dplyr::collect()
   
   ## aggregate to daily
   met_future <- met_future %>% 
     mutate(datetime = lubridate::as_date(datetime)) %>% 
-    group_by(datetime, site_id, parameter) |> 
-    summarize(air_temperature = mean(prediction), .groups = "drop") |> 
+    group_by(datetime, site_id, parameter, variable) |> 
+    summarize(prediction = mean(prediction),.groups = "drop") |> 
     #    mutate(air_temperature = air_temperature - 273.15) |> 
-    select(datetime, site_id, air_temperature, parameter)
+    select(datetime, site_id, variable, prediction, parameter)
   
-  met_future <- met_future %>%
-    tidyr::pivot_longer(
-      cols = 3, 
-      names_to="variable",
-      values_to = "prediction"
-    )
+  #met_future <- met_future %>%
+   # tidyr::pivot_longer(
+    #  cols = 3, 
+     # names_to="variable",
+      #values_to = "prediction"
+    #)
   
   return(met_future)
 }
 
+#target     <- merge_met_past(target)   ## append met data (X) into target file
+met_future <- download_met_forecast(forecast_date) 
+unique(met_future$variable)
+
 ##' append historical meteorological data into target file
-df_past <- neon4cast::noaa_stage3()
+df_past <- neon4cast::noaa_stage3() %>%
+  dplyr::filter(site_id %in% sites,
+                lubridate::year(datetime) >= 2020,
+                variable %in% c("air_pressure", "air_temperature",
+                                "relative_humidity", "preciptation_flux")) |> 
+  dplyr::collect()
 
 #check for one site
 d_site1 <- df_past |> 
   dplyr::filter(site_id == "BART") |>
   dplyr::collect()
 
-# Try for all sites
-noaa_all <- df_past |> 
-  dplyr::filter(site_id %in% sites) |>
-  dplyr::collect() # this won't work on local hard drive, can't allocate 2,6Gb of data
+df_past <- df_past %>%
+  select(-c(family, reference_datetime))
 
-noaa_all <- noaa_all %>%
-  select(-c(family, reference_datetime)) 
-
-merged_noaa <- rbind(met_future, noaa_all) 
-
-#target     <- merge_met_past(target)   ## append met data (X) into target file
-met_future <- download_met_forecast(forecast_date) 
+merged_noaa <- rbind(met_future, df_past) 
 
 ## visual check of data
+# Make time series of LE
 target1 %>% 
   filter(variable=="le",
          site_id=="BART") %>%
@@ -93,6 +95,7 @@ target1 %>%
   #facet_grid(~site_id, scale ="free") +
   ggtitle("LE")
 
+# Make time series of NEE
 target1 %>% 
   filter(variable=="nee",
          site_id=="BART") %>%
@@ -101,13 +104,21 @@ target1 %>%
   #facet_grid(~site_id, scale ="free") +
   ggtitle("NEE")
 
+#Make time series of all future NOAA
 met_future %>%
   filter(site_id=="BART") %>%
   ggplot(aes(x=datetime, y=air_temperature)) +
   geom_point() +
-  ggtitle("NOAA Air Temp")
+  ggtitle("NOAA Air Temp, March - April 2024")
 
-
+# Make timeseries of ALL past and future NOAA
+unique(merged_noaa$variable)
+merged_noaa %>%
+  filter(site_id=="BART") %>%
+  ggplot(aes(x=datetime, y=prediction)) +
+  geom_point() +
+  facet_wrap(~variable) +
+  ggtitle("NOAA Weather Variables, 2020-2024")
 
 # Make plots of NOAA covariate data
 temp = d_site1[d_site1$variable == "air_temperature",]
