3d9da988b0efbf53b9e9f9e6079b645dadf0b397
bvanloen <157618334+bvanloen@users.noreply.github.com>
2024-03-07 21:09:43 -0500

    adding future NOAA data to the script + met timeseries

--- Data_Download_Functions/01_datatargetdownload.R
+++ Data_Download_Functions/01_datatargetdownload.R
@@ -21,6 +21,37 @@ download_site_meta <- function(){
   site_data %>% filter(as.integer(terrestrial) == 1)
 }
 
+# Get future NOAA data
+forecast_date <- Sys.Date()
+noaa_date <- Sys.Date() - days(1)  #Need to use yesterday's NOAA forecast because today's is not available yet
+
+##' Download NOAA GEFS weather forecast
+##' @param forecast_date start date of forecast
+##' @return dataframe
+download_met_forecast <- function(forecast_date){
+  noaa_date <- forecast_date - lubridate::days(1)  #Need to use yesterday's NOAA forecast because today's is not available yet
+  
+  ## connect to data
+  df_future <- neon4cast::noaa_stage2(start_date = as.character(noaa_date))
+  
+  ## filter available forecasts by date and variable
+  met_future <- df_future |> 
+    dplyr::filter(datetime >= lubridate::as_datetime(forecast_date), 
+                  variable == "air_temperature") |> 
+    dplyr::collect()
+  
+  ## aggregate to daily
+  met_future <- met_future %>% 
+    mutate(datetime = lubridate::as_date(datetime)) %>% 
+    group_by(datetime, site_id, parameter) |> 
+    summarize(air_temperature = mean(prediction), .groups = "drop") |> 
+    #    mutate(air_temperature = air_temperature - 273.15) |> 
+    select(datetime, site_id, air_temperature, parameter)
+  
+  return(met_future)
+}
+
+
 ##' append historical meteorological data into target file
 ##' @param target targets dataframe
 ##' @return updated targets dataframe with added weather data
@@ -63,13 +94,14 @@ merge_met_past <- function(target){
   
 }
 
-target     <- download_targets()       ## Y variables
+target1 <- download_targets()       ## Y variables
+sites <- unique(target$site_id)
 site_data  <- download_site_meta()
 #target     <- merge_met_past(target)   ## append met data (X) into target file
-#met_future <- download_met_forecast(forecast_date) 
+met_future <- download_met_forecast(forecast_date) 
 
 ## visual check of data
-target %>% 
+target1 %>% 
   filter(variable=="le",
          site_id=="BART") %>%
   ggplot(aes(x = datetime, y = observation)) +
@@ -77,7 +109,7 @@ target %>%
   #facet_grid(~site_id, scale ="free") +
   ggtitle("LE")
 
-target %>% 
+target1 %>% 
   filter(variable=="nee",
          site_id=="BART") %>%
   ggplot(aes(x = datetime, y = observation)) +
@@ -85,4 +117,9 @@ target %>%
   #facet_grid(~site_id, scale ="free") +
   ggtitle("NEE")
 
+target %>%
+  filter(site_id=="BART") %>%
+  ggplot(aes(x=datetime, y=air_temperature)) +
+  geom_point() +
+  ggtitle("NOAA Air Temp")
 
