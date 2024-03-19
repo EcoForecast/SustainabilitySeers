a4280d623c59237e1407b54b0a89f1049ea1fbd9
Yixue Meng <yixue@yixue-13.myfiosgateway.com>
2024-03-19 17:55:48 -0400

    Add diagnostics.

--- 02_calibration.R
+++ 02_calibration.R
@@ -14,10 +14,9 @@ sw <- ncdf4::ncvar_get(nc, "surface_downwelling_shortwave_flux_in_air")
 
 #grab NEE
 nee.df <- readr::read_csv("https://sdsc.osn.xsede.org/bio230014-bucket01/challenges/targets/project_id=neon4cast/duration=PT30M/terrestrial_30min-targets.csv.gz", guess_max = 1e6)
-nee.df <- nee.df %>% dplyr::filter(site_id == "HARV", 
-         datetime >= as.Date("2021-01-01"), 
-         datetime < as.Date("2022-01-01"), 
-         variable == "nee")
+nee.df <- nee.df[which(nee.df$site_id == "HARV" & 
+                         lubridate::year(nee.df$datetime) == "2021" & 
+                         nee.df$variable == "nee"),]
 
 #match time for nee
 realtime <- as.POSIXct(nc$dim$time$vals*3600, origin = "2021-01-01", tz = "UTC")
@@ -49,21 +48,17 @@ data <- list(x_ic = mean(nee_daily, na.rm = T),
              Precip = precip_daily,
              Temp = temp_daily,
              SW = sw_daily)
-#convert to anomalies
-# data$Precip <- data$Precip - mean(data$Precip)
-# data$Temp <- data$Temp - mean(data$Temp)
-# data$SW <- data$SW - mean(data$SW)
-
-#normalize
-# data$Precip <- (data$Precip-min(data$Precip))/(max(data$Precip)-min(data$Precip))*100
 
 ef.out <- ecoforecastR::fit_dlm(model=list(obs="y",fixed="~ 1 + X + Temp + Precip + SW"), data)
 out <- do.call(rbind, ef.out$predict)
 x.cols <- grep("^x",colnames(out)) ## grab all columns that start with the letter x
 ci <- apply(out[,x.cols],2,quantile,c(0.025,0.5,0.975)) ## model was fit on log scale
-plot(days, ci[3,], type="l", ylim = range(nee_daily, na.rm = T), col=1)
 
 #time series plot
+plot(days, ci[3,], type="l", ylim = range(nee_daily, na.rm = T), col=1)
 lines(days, ci[1,], col=1)
 lines(days, nee_daily, col=2)
 legend("bottomleft", legend=c("CI", "NEE Observation"), lty=1, col=c(1,2))
+
+#diagnostics
+BGR <- gelman.plot(ef.out$params)
